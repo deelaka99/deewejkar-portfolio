84e20c02dd601a28e8488a779df8ac53d81e476f
deelaka99 <123wejith@gmail.com>
2023-07-03 00:09:25 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <h1>dee</h1>
       <Router>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/projects' element={<Projects/>} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='toggleButton'>
+        <button></button>
+      </div>
+      <div className='links'>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experiences">Experiences</Link>
+      </div>
+    </div>
+  )
+}
