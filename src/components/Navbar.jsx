d95d2dbd2bb24d0cf5e98dcbfc9e49d04484920a
deelaka99 <123wejith@gmail.com>
2023-07-03 00:18:41 +0530

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 export default function Navbar() {
   return (
     <div className='navbar'>
       <div className='toggleButton'>
-        <button></button>
+        <button>
+            <ReorderIcon/>
+        </button>
       </div>
       <div className='links'>
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
-        <Link to="/experiences">Experiences</Link>
+        <Link to="/experience">Experiences</Link>
       </div>
     </div>
   )
