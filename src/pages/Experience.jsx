122ce8455df0dfac5be395ccfb0a163bde8f684c
deelaka99 <123wejith@gmail.com>
2023-07-03 02:04:51 +0530

    seventh commit

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
 
 function Experience() {
   return (
-    <div>
-      <h1>Experience</h1>
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon/>}
+        >
+          <h3 className="vertical-timeline-element-title">
+            My Random High school, Random Place, Random state
+          </h3>
+          <p>High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon/>}
+        >
+          <h3 className="vertical-timeline-element-title">
+            My Random High school, Random Place, Random state
+          </h3>
+          <p>High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon/>}
+        >
+          <h3 className="vertical-timeline-element-title">
+            My Random High school, Random Place, Random state
+          </h3>
+          <p>High School Diploma</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import EmailIcon from "@mui/icons-material/Email";
+import GithubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      <div className="about">
+        <h2>Hi, My name is Deelaka</h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <LinkedInIcon/>
+          <EmailIcon/>
+          <GithubIcon/>
+        </div>
+      </div>
+      <div className="skills">
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>
+              ReactJS, HTML, CSS, Bootstrap, TailwindCSS, MUI
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              Laravel, PHP, NodeJS, MySQL 
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>
+              Java, JS, Python, C#, C, TypeScript
+            </span>
+          </li>
+        </ol>
+      </div>
     </div>
   )
 }
