b1f3e278baabfc6a9c2f446f6fcbce5abdad189c
deelaka99 <123wejith@gmail.com>
2023-07-03 00:24:52 +0530

    fifth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
 
 export default function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(()=>{
+    setExpandNavbar(false);
+  },[location]);
+
   return (
-    <div className='navbar'>
-      <div className='toggleButton'>
-        <button>
-            <ReorderIcon/>
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
         </button>
       </div>
-      <div className='links'>
+      <div className="links">
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/experience">Experiences</Link>
       </div>
     </div>
-  )
+  );
 }
