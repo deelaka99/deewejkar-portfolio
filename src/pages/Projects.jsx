eca305ced6d2c20fb37a8db853145fa98c4c78c0
deelaka99 <123wejith@gmail.com>
2023-07-03 12:54:15 +0530

    Eight commit

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import ProjImg1 from '../assets/p1.jpeg'
+import ProjImg2 from '../assets/p2.png'
+import '../styles/Project.css';
 
 function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className='projects'>
+      <h1>My Personal Projects</h1>
+      <div className='projectList'>
+        <ProjectItem name="Fit360 Fitness Tracker App" image={ProjImg1}/>
+        <ProjectItem name="Restaurent Management System" image={ProjImg2}/>
+      </div>
     </div>
   )
 }
