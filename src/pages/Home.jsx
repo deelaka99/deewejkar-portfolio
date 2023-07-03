6e92c04270f43321b70eecded82319d827bbb7e4
deelaka99 <123wejith@gmail.com>
2023-07-03 16:46:01 +0530

    11th commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import EmailIcon from "@mui/icons-material/Email";
+import YoutubeIcon from "@mui/icons-material/YouTube";
 import GithubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "../styles/Home.css";
@@ -8,12 +8,12 @@ function Home() {
   return (
     <div className='home'>
       <div className="about">
-        <h2>Hi, My name is Deelaka</h2>
+        <h2>Hi, My name is Deelaka Wejtih</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
-          <LinkedInIcon/>
-          <EmailIcon/>
-          <GithubIcon/>
+          <a href=""><LinkedInIcon/></a>          
+          <a href=""><GithubIcon/></a>
+          <a href=""><YoutubeIcon/></a>
         </div>
       </div>
       <div className="skills">
