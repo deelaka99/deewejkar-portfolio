bab7efa8579f811d034ed95ddd48af2b6e9114ba
deelaka99 <123wejith@gmail.com>
2023-12-01 23:25:31 +0530

    added some changes to the ProjectList and project

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -1,6 +1,6 @@
 import ProjImg1 from "../assets/1.png";
 import ProjImg2 from "../assets/3.png";
-import ProjImg3 from "../assets/p3.png";
+import ProjImg3 from "../assets/9.png";
 import ProjImg4 from "../assets/6.png";
 import ProjImg5 from "../assets/2.png";
 import ProjImg6 from "../assets/4.png";
@@ -10,16 +10,24 @@ import ProjImg9 from "../assets/8.png";
 
 export const ProjectList = [
   {
-    name: "Laborties Management System",
+    name: "DTM Laborties Management System",
     image: ProjImg5,
     skills: "ReactJS, Firebase, TailwindCSS, HTML, JS, CSS",
     specialization: "Web",
+    desc: "This project was a pivotal component of my university's mini-project, where I took on a lead role in managing two integral sites. Focused on enhancing laboratory management in Sri Lanka, this specific site delivers comprehensive functionality, including Create, Read, Update, and Delete (CRUD) operations for both laboratories and users. The primary objective of the platform is to streamline local laboratory management, and it achieves this through robust features. The system enables seamless handling of labs and user data, allowing for efficient CRUD operations. Additionally, the platform prioritizes security with built-in features such as user authentication and password management, providing a secure and user-friendly experience. As part of this initiative, I embraced the responsibility of overseeing and implementing these critical features. Throughout the development process, I navigated the complexities of user authentication, ensuring the system's integrity. Furthermore, the platform incorporates a user-friendly interface for password management, facilitating a convenient and secure means for users to update their passwords. This project not only showcases my technical proficiency in implementing CRUD functionalities but also underscores my commitment to ensuring secure and efficient data management in the context of laboratory administration",
+    figma : "https://www.figma.com/file/ynQFXTGAoRdczP9VYIIkxx/Company-Admin-dashboard?type=design&node-id=0-1&mode=design&t=yzqAJrGmvAF4aCBp-0",
+    github: "https://github.com/deelaka99/company-admin-panel-main",
+    activelink: "https://dtm-admin-panel.netlify.app/",
   },
   {
     name: "SharpTester Lab Management System",
     image: ProjImg8,
     skills: "ReactJS, Firebase, TailwindCSS, HTML, JS, CSS",
     specialization: "Web",
+    desc: "This project was a key undertaking within my University Mini Project, where I assumed a leadership role overseeing the development of two integral sites. As part of this collaborative group effort, I took responsibility for the design and implementation of both platforms, and this description pertains to one of those sites. The primary focus of this site is to facilitate the efficient management of users and their associated reports within a specific laboratory in Sri Lanka. Aiming for comprehensive functionality, the platform incorporates essential CRUD operations for both labs and users. Additionally, the system prioritizes security with features such as user authentication and password management, ensuring the confidentiality and integrity of user data. Noteworthy features include a user-friendly dashboard designed to provide analytical insights. This dashboard serves as a centralized hub for users, offering an intuitive interface to explore and understand key analytical details related to the managed data. As the lead contributor to this aspect of the mini project, I navigated the complexities of developing a robust user management system with security features and an analytical dashboard, enhancing the overall functionality and usability of the platform.",
+    figma : "https://www.figma.com/file/japBwlqRJzUXl6XM3lQJap/Laboratory-Dashboard?type=design&node-id=0%3A1&mode=design&t=wkyTHeFT9MO7oV3u-1",
+    github: "https://github.com/deelaka99/lab-admin-panel",
+    activelink: "https://sharptester-lms.netlify.app/",
   },
   {
     name: "Restaurent Management System",
@@ -27,18 +35,30 @@ export const ProjectList = [
     skills:
       "HTML, CSS, JavaScript, PHP, SQL, Bootstrap, Laravel, MySQL, PhpMyAdmin",
     specialization: "Web",
+    desc: "This project represents a significant milestone in my academic journey as it was part of a University Mini Project undertaken in the second year. Serving as a collaborative group effort, I assumed the pivotal role of overseeing the development of the entire site.The project is structured into two distinct components: the Admin Dashboard and the User Portal. The Admin Dashboard empowers administrators with comprehensive CRUD (Create, Read, Update, Delete) functionalities. Admins can effortlessly manage the platform by adding and deleting food items, incorporating new chefs, receiving and reviewing orders, and performing various administrative tasks. On the other hand, the User Portal provides a seamless experience for users. They can effortlessly add items to their cart and submit orders, streamlining the process of selecting and purchasing items from the platform. As the primary developer, my responsibilities extended to crafting both the Admin Dashboard and the User Portal, ensuring that each component fulfilled its designated functions effectively. This project not only honed my technical skills but also provided valuable experience in collaborative development within a team setting.",
+    figma : false,
+    github: "https://github.com/deelaka99/Restaurent_Management_System",
+    activelink: false,
   },
   {
     name: "Portfolio with Dark theme",
     image: ProjImg7,
     skills: "ReactJS, HTML, CSS, JavaScript",
     specialization: "Web",
+    desc: "This project marks a significant milestone as my inaugural venture into ReactJS development. Undertaking this project proved to be an invaluable learning experience, allowing me to gain substantial insights into the intricacies of ReactJS and Tailwind CSS. Despite the realization that the project may not have achieved the level of excellence I initially envisioned, the journey was nonetheless enriching, and I consider the outcome to be a commendable achievement. This project served as a foundational step in my ReactJS journey, laying the groundwork for future endeavors and showcasing my dedication to continuous learning and improvement.",
+    figma : false,
+    github: "https://github.com/deelaka99/dee-wej-kar-portfolio",
+    activelink: "https://dee-wej-kar.netlify.app/",
   },
   {
     name: "My Bio Website",
     image: ProjImg4,
     skills: "HTML, CSS, JavaScript, PHP, Bootstrap, Adobe Photoshop",
     specialization: "Web",
+    desc: "This portfolio represents my initial foray into showcasing my skills and creative endeavors. It features a collection of my artworks, photography, and a dedicated site for my mobile photography. While it may be modest in its scope, this portfolio serves as a meaningful introduction to my artistic expressions and passion for photography. As my inaugural portfolio, it reflects the beginning of my journey in presenting my work to a wider audience, setting the stage for future expansions and refinements.",
+    figma : false,
+    github: "https://github.com/deelaka99/myBioWebsite",
+    activelink: "https://deelaka99.github.io/myBioWebsite/",
   },
 
   {
@@ -46,23 +66,41 @@ export const ProjectList = [
     image: ProjImg6,
     skills: "HTML, CSS, JavaScript, PHP",
     specialization: "Web",
+    desc: "This commercial website stands as a testament to my inaugural venture into the world of web development, serving as my first project for a shopping mall. Executed as part of my first-year mini project at OUSL, this website was crafted based on my original design and implementation plan. Taking on the challenge of creating a digital presence for a shopping mall allowed me to apply theoretical knowledge gained in my studies and embark on a hands-on learning experience. This project marks a pivotal moment in my journey, setting the foundation for subsequent web development endeavors and fostering a passion for creating impactful online solutions.",
+    figma : false,
+    github: "https://github.com/deelaka99/Glamoure.360",
+    activelink: "https://deelaka99.github.io/Glamoure.360/",
   },
   {
     name: "Fit360 Fitness Tracker App",
     image: ProjImg1,
     skills: "Java, XML, Android Studio",
     specialization: "Mobile",
+    desc: "The Fitness Tracking App is a holistic solution designed for individuals keen on enhancing their fitness and overall health. This application empowers users to seamlessly monitor and elevate their fitness levels through features such as workout tracking, goal setting, progress monitoring, and insightful analytics. Engineered to be user-friendly and intuitive, the app caters to users of all fitness backgrounds, ensuring accessibility and usability. Developed using Android Studio and the Java programming language, this application reflects a commitment to delivering a robust and user-centric fitness experience.",
+    figma : false,
+    appLink: "https://drive.google.com/file/d/1LJ7R3UIVlxWm7oEVRuCpq0RSHXrvjvY_/view?usp=sharing",
+    github: "https://github.com/deelaka99/Fit360",
+    activelink: false,
   },
   {
     name: "To_Do_List app",
     image: ProjImg3,
-    skills: "Java, XML, Android Studio",
+    skills: "Java, XML, Android Studio, SQLite",
     specialization: "Mobile",
+    desc: "The To-Do App, crafted with Android Studio and powered by SQLite, represents an assignment undertaken during my second year in the OUSL Android subject. This application serves as a practical implementation of fundamental Android development concepts, featuring essential CRUD (Create, Read, Update, Delete) operations for the seamless management of daily tasks. Designed with simplicity in mind, the app provides an intuitive user experience, demonstrating my proficiency in mobile app development and database integration.",
+    figma : false,
+    appLink: false,
+    github: "https://github.com/deelaka99/To_Do_List",
+    activelink: false,
   },
   {
     name: "Coffee-machine",
     image: ProjImg9,
     skills: "Python",
     specialization: "Code",
+    desc: "This project, undertaken during my first year in the OUSL Python subject, represents a mini project that delves into the realms of object-oriented programming (OOP). Specifically, the project features a code-line-based interface designed for the operation of a coffee machine. Leveraging OOP concepts, I crafted a robust and efficient system that showcases my understanding and application of Python programming principles. Through this endeavor, I not only gained valuable insights into Python development but also honed my skills in designing effective and structured solutions.",
+    figma : false,
+    github: "https://github.com/deelaka99/Coffee-machine",
+    activelink: false,
   },
 ];
--- src/pages/ProjectDisplay.jsx
+++ src/pages/ProjectDisplay.jsx
@@ -2,20 +2,92 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList";
 import GithubIcon from "@mui/icons-material/GitHub";
+import PublicIcon from "@mui/icons-material/Public";
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
   const { id } = useParams();
   const project = ProjectList[id];
 
+  const handleFigmaClick = () => {
+    const figmaLink = project.figma;
+
+    // Open the link in a new tab
+    window.open(figmaLink, "_blank");
+  };
+
+  const handleAppLinkClick = () => {
+    const appLink = project.appLink;
+
+    // Open the link in a new tab
+    window.open(appLink, "_blank");
+  };
+
+  const handleGithubClick = () => {
+    const githubLink = project.github;
+
+    // Open the link in a new tab
+    window.open(githubLink, "_blank");
+  };
+
+  const handleActiveSiteClick = () => {
+    const activeSiteLink = project.activelink;
+
+    // Open the link in a new tab
+    window.open(activeSiteLink, "_blank");
+  };
+
   return (
     <div className="project">
-      <h1>{project.name}</h1>
+      <h1 style={{ fontFamily: "monospace" }}>{project.name}</h1>
       <img src={project.image} alt="project image" />
       <p>
-        <b>Skills:</b> {project.skills}
+        <b>Specialization:</b> {project.specialization}
       </p>
-      <GithubIcon />
+      <div className="about-project">
+        <p>
+          <b>Langauges & Tools used:</b>
+          {project.skills}
+        </p>
+        <p>
+          <b>
+            <center style={{ fontSize: "larger", textDecoration: "underline" }}>
+              About the project
+            </center>
+          </b>
+          <br /> {project.desc}
+        </p>
+
+        {project.figma ? (
+          <p>
+            <b className="figmaLink" onClick={handleFigmaClick}>
+              Figma Link
+            </b>
+          </p>
+        ) : null}
+        {project.specialization === "Mobile" && project.appLink ? (
+          <p>
+            <b className="figmaLink" onClick={handleAppLinkClick}>
+              Download APK
+            </b>
+          </p>
+        ) : null}
+      </div>
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <GithubIcon
+          className="figmaLink"
+          style={{ fontSize: "40px" }}
+          onClick={handleGithubClick}
+        />
+        {project.activelink ? (
+          <PublicIcon
+            className="figmaLink"
+            style={{ fontSize: "40px" }}
+            onClick={handleActiveSiteClick}
+          />
+        ) : null}
+      </div>
     </div>
   );
 }
