8494573e450670cd66f70938fc4e501d40b72184
deelaka99 <123wejith@gmail.com>
2023-12-29 01:02:12 +0530

    changed styling of special moments

--- src/pages/SpecialMoments.jsx
+++ src/pages/SpecialMoments.jsx
@@ -32,7 +32,7 @@ import Img21 from "../assets/specialMoments/2023_09_20.jpg";
 import Img22 from "../assets/specialMoments/2023_10_13.jpg";
 import Img23 from "../assets/specialMoments/2020_09_03.jpg";
 
-import '../styles/specialMoments.css';
+import "../styles/specialMoments.css";
 
 function Experience() {
   return (
@@ -44,14 +44,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img22} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img22} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Mind blowing Vega Electric Car
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Vega Innovations, Trace Expertcity, Maradana.
           </p>
@@ -62,14 +61,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img21} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img21} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             With my winning team of Innova 2k23
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Faculty of Management Studies, SUSL.
           </p>
@@ -80,14 +78,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img20} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img20} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Award ceremony of Innova 2k23
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Faculty of Management Studies, SUSL.
           </p>
@@ -98,14 +95,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img19} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img19} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             3rd Place of IT category of Innova 2k23
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Faculty of Management Studies, SUSL.
           </p>
@@ -116,14 +112,11 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img18} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
-            JumborIEEE 2k23
-          </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <div className="imageHolder">
+            <img src={Img18} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">JumborIEEE 2k23</h3>
+          <p>
             <LocationIcon />
             Uva Wellassa University of Sri Lanka.
           </p>
@@ -134,14 +127,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img17} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img17} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             IEEE Nenasa 2k23 | Mahiyanaganaya School Visit
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             B/ Kandekapu Ulpatha Maha Vidyalaya
           </p>
@@ -152,16 +144,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img16} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img16} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Manager of Media Division of SedsSabra
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
-            AGM of SedsSabra 2023.
-          </p>
+          <p>AGM of SedsSabra 2023.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
@@ -169,14 +158,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img15} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img15} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             InfoBash Cricket Tourenment
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Hunduwa Ground, Sabaragamuwa University of Sri Lanka.
           </p>
@@ -187,14 +175,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img14} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img14} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Faculty of Computing's Opening Ceremony
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Faculty of Computing, Sabaragamuwa University of Sri Lanka.
           </p>
@@ -205,16 +192,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img13} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img13} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Volunteers of the Month
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
-            IEEE Student branch of SUSL
-          </p>
+          <p>IEEE Student branch of SUSL</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
@@ -222,16 +206,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img12} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img12} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             The Semi-Finalists of SpaceHack 2021
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
-            Organized by SedsSabra.
-          </p>
+          <p>Organized by SedsSabra.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
@@ -239,14 +220,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img23} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img23} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Student Registration Day of SUSL
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Sabaragamuwa University of Sri Lanka.
           </p>
@@ -257,14 +237,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img11} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img11} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             End Examination Day at OUSL
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             The Libaray of the Open University of Sri Lanka.
           </p>
@@ -275,14 +254,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img10} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img10} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Final Day as a DS Student
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             D.S. Senanayake College, Colombo 07.
           </p>
@@ -293,14 +271,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img9} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img9} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             With Open Uni friends
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Weli Park, Nawala, Nugegoda.
           </p>
@@ -311,14 +288,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img8} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img8} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             School Leavers with ICT madam
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             D.S. Senanayake College, Colombo 07.
           </p>
@@ -329,14 +305,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img7} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img7} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             A/L Results Realesed Day
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             D.S. Senanayake College, Colombo 07.
           </p>
@@ -347,14 +322,11 @@ function Experience() {
           iconStyle={{ background: "#DCC825", color: "#fff" }}
           icon={<WorkIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img6} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
-            Final day at OIT
-          </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <div className="imageHolder">
+            <img src={Img6} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">Final day at OIT</h3>
+          <p>
             <LocationIcon />
             Head office, OrelIT, Nawinna, Maharagama.
           </p>
@@ -365,14 +337,13 @@ function Experience() {
           iconStyle={{ background: "#DCC825", color: "#fff" }}
           icon={<WorkIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img5} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img5} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Training period of OIT
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Head office, OrelIT, Nawinna, Maharagama.
           </p>
@@ -383,14 +354,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img4} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img4} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Final day of the A/L examination
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             D.S. Senanayake College, Colombo 07.
           </p>
@@ -401,14 +371,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img3} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img3} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Senani Educational Exibition 2017
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             D.S. Senanayake College, Colombo 07.
           </p>
@@ -419,14 +388,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img2} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img2} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             Annual Prize Giving Ceremony 2015
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             Youth Center, Maharagama.
           </p>
@@ -437,14 +405,13 @@ function Experience() {
           iconStyle={{ background: "#021f6a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
-          <img style={{ borderRadius: "5px" }} src={Img1} alt="profileDp" />
-          <h3
-            style={{ color: "#021f6a" }}
-            className="vertical-timeline-element-title"
-          >
+          <div className="imageHolder">
+            <img src={Img1} alt="profileDp" />
+          </div>
+          <h3 className="vertical-timeline-element-title">
             After the End of O/L examination
           </h3>
-          <p style={{ color: "gray", fontFamily: "monospace" }}>
+          <p>
             <LocationIcon />
             President's College, Maharagama.
           </p>
