1795f5af883fb7fbd6edc16e0436ef9324f621ac
deelaka99 <123wejith@gmail.com>
2023-12-28 12:32:09 +0530

    make navbar some changes and set its position as fixed

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <div className='App'>
       <Router>
         <Navbar/>
+        <div style={{height:"100px",}}></div>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/projects' element={<Projects/>} />
