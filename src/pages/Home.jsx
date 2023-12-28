8e8e1ccadf443ea66c75e252671105c04684b8ac
deelaka99 <123wejith@gmail.com>
2023-12-28 19:18:01 +0530

    added langauges slider to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,36 @@
-import React from 'react'
+import React from "react";
 import YoutubeIcon from "@mui/icons-material/YouTube";
 import GithubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import ProfileDP from "../assets/dp2.png";
+import Languages from "../components/Languages";
+import Certifications from "../components/Certifications";
 import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className='home'>
+    <div className="home">
       <div className="about">
-        <img src={ProfileDP} alt="profileDp"/>
+        <img src={ProfileDP} alt="profileDp" />
         <h2>Hi, I am Deelaka Wejth</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
-          <a href="https://www.linkedin.com/in/deelaka-wejith-kariyawasam-7a9bb0213/"><LinkedInIcon/></a>          
-          <a href="https://github.com/deelaka99"><GithubIcon/></a>
-          <a href="https://www.youtube.com/@deelakawejithkariyawasam"><YoutubeIcon/></a>
+          <a href="https://www.linkedin.com/in/deelaka-wejith-kariyawasam-7a9bb0213/">
+            <LinkedInIcon />
+          </a>
+          <a href="https://github.com/deelaka99">
+            <GithubIcon />
+          </a>
+          <a href="https://www.youtube.com/@deelakawejithkariyawasam">
+            <YoutubeIcon />
+          </a>
         </div>
       </div>
       <div className="skills">
-        <ol className="list">
-          <li className="item">
-            <h2>Front-End</h2>
-            <span>
-              ReactJS, HTML, CSS, JS, Bootstrap, TailwindCSS
-            </span>
-          </li>
-          <li className="item">
-            <h2>Back-End</h2>
-            <span>
-              Laravel, PHP, NodeJS, MySQL, MongoDB 
-            </span>
-          </li>
-          <li className="item">
-            <h2>Languages</h2>
-            <span>
-              Java, Python, C#, C
-            </span>
-          </li>
-        </ol>
+        <Languages/>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
