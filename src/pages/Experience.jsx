53f52a89e78df9ab5b43b7d8692e0863835fd111
deelaka99 <123wejith@gmail.com>
2023-12-28 13:01:16 +0530

    added some changes to the experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -10,102 +10,156 @@ import WorkIcon from "@mui/icons-material/Work";
 function Experience() {
   return (
     <div className="experience">
-      <VerticalTimeline lineColor="#3e497a">
+      <VerticalTimeline lineColor="#021f6a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2005 - 2009"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon/>}
+          date="2020 - present"
+          iconStyle={{ background: "#021f6a", color: "#fff" }}
+          icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Homagama Maha Vidyala, Homagama
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            Sabaragamuwa University of Sri Lanka
           </h3>
-          <p>Grade 5 Scholorship</p>
+          <h4
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            Faculty of Computing
+          </h4>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Undergraduate of{" "}
+            <b>
+              Bacherlor of Science in Computing & Information Systems (Special)
+              Degree Program
+            </b>
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2010 - 2015"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon/>}
+          date="2020 - 2021"
+          iconStyle={{ background: "#DCC825", color: "#fff" }}
+          icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            President's College, Maharagama
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            ScafLanka Solutions
           </h3>
-          <p>G.C.E. Ordinary Level Examination</p>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Worked as a <b>Junior Graphic Designer</b>
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2016 - 2018"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon/>}
+          date="2019 - present"
+          iconStyle={{ background: "#021f6a", color: "#fff" }}
+          icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            D.S. Senanayake College, Colombo 07 
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            The Open University of Sri Lanka
           </h3>
-          <p>G.C.E. Advanced Level Examination (Physical science stream + ICT)</p>
+          <h4
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            Faculty of Engineering Technology
+          </h4>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Undergraduate of{" "}
+            <b>Bacherlor of Software Engineering (Honurs) Degree Program</b>
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019 - 2021"
+          iconStyle={{ background: "#021f6a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            ESOFT Metro Campus, Nugegoda
+          </h3>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Diploma in Information Technology
+          </p>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Diploma in English
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2018 - 2019"
           iconStyle={{ background: "#DCC825", color: "#fff" }}
-          icon={<WorkIcon/>}
+          icon={<WorkIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            OrelIT Coperation, Nawinna, Maharagama 
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            OrelIT Coperation, Nawinna, Maharagama
           </h3>
-          <p>Worked as a <b>Associate Image Processor</b></p>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Worked as a <b>Associate Image Processor</b>
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2019 - 2021"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon/>}
+          date="2016 - 2018"
+          iconStyle={{ background: "#021f6a", color: "#fff" }}
+          icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            ESOFT Metro Campus, Nugegoda 
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            D.S. Senanayake College, Colombo 07
           </h3>
-          <p>Diploma in Information Technology</p>
-          <p>Diploma in English</p>
-        </VerticalTimelineElement>         
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2019 - present"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon/>}
-        >
-          <h3 className="vertical-timeline-element-title">
-            The Open University of Sri Lanka 
-          </h3>
-          <h4 className="vertical-timeline-element-title">
-            Faculty of Engineering Technology 
-          </h4>
-          <p>Undergraduate of <b>Bacherlor of Software Engineering (Honurs) Degree Program</b></p>
-        </VerticalTimelineElement>         
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2020 - 2021"
-          iconStyle={{ background: "#DCC825", color: "#fff" }}
-          icon={<WorkIcon/>}
-        >
-          <h3 className="vertical-timeline-element-title">
-            ScafLanka Solutions 
-          </h3>
-          <p>Worked as a <b>Junior Graphic Designer</b></p>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            G.C.E. Advanced Level Examination (Physical science stream + ICT)
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2020 - present"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<SchoolIcon/>}
+          date="2010 - 2015"
+          iconStyle={{ background: "#021f6a", color: "#fff" }}
+          icon={<SchoolIcon />}
         >
-          <h3 className="vertical-timeline-element-title">
-            Sabaragamuwa University of Sri Lanka 
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            President's College, Maharagama
           </h3>
-          <h4 className="vertical-timeline-element-title">
-            Faculty of Computing 
-          </h4>
-          <p>Undergraduate of <b>Bacherlor of Science in Computing & Information Systems (Special) Degree Program</b></p>
-        </VerticalTimelineElement> 
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            G.C.E. Ordinary Level Examination
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2005 - 2009"
+          iconStyle={{ background: "#021f6a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3
+            style={{ color: "#021f6a" }}
+            className="vertical-timeline-element-title"
+          >
+            Homagama Maha Vidyala, Homagama
+          </h3>
+          <p style={{ color: "gray", fontFamily: "monospace" }}>
+            Grade 5 Scholorship
+          </p>
+        </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   );
