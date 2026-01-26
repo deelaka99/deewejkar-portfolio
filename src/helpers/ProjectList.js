f08a987c5209b0d1335177d5873f46817676c45e
deelaka99 <123wejith@gmail.com>
2026-01-26 18:48:49 +0530

    Created ProjectList helper

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,254 @@
+import ProjImg1 from "../images/1.png";
+import ProjImg1_1 from "../images/16.png";
+import ProjImg2 from "../images/3.png";
+import ProjImg2_1 from "../images/15.png";
+import ProjImg3 from "../images/9.png";
+import ProjImg3_1 from "../images/17.png";
+import ProjImg4 from "../images/6.png";
+import ProjImg4_1 from "../images/13.png";
+import ProjImg5 from "../images/2.png";
+import ProjImg5_1 from "../images/10.png";
+import ProjImg6 from "../images/4.png";
+import ProjImg6_1 from "../images/14.png";
+import ProjImg7 from "../images/5.png";
+import ProjImg7_1 from "../images/12.png";
+import ProjImg8 from "../images/7.png";
+import ProjImg8_1 from "../images/11.png";
+import ProjImg9 from "../images/8.png";
+import ProjImg9_1 from "../images/18.png";
+import ProjImg10 from "../images/20.png";
+import ProjImg10_1 from "../images/19.png";
+import ProjImg11 from "../images/21.png";
+import ProjImg11_1 from "../images/22.png";
+import ProjImg12 from "../images/23.png";
+import ProjImg12_1 from "../images/23.png";
+import ProjImg13 from "../images/24.png";
+import ProjImg13_1 from "../images/25.png";
+import ProjImg14 from "../images/26.png";
+import ProjImg14_1 from "../images/27.png";
+import ProjImg15 from "../images/28.png";
+import ProjImg15_1 from "../images/29.png";
+import ProjImg16 from "../images/30.png";
+import ProjImg16_1 from "../images/31.png";
+
+export const ProjectList = [
+  {
+    name: "Starbuck POS System",
+    image: ProjImg10,
+    image1: ProjImg10_1,
+    skills: "ReactJS, TailwindCSS, HTML, JS, CSS",
+    specialization: "Web",
+    desc: "StarBuck is a personalized Point of Sale (POS) system meticulously crafted by me using cutting-edge technologies such as ReactJS and TailwindCSS frameworks. The development process began with the creation of a comprehensive wireframe using Figma, ensuring a thoughtful and user-friendly design. Thorough attention was given to tablet devices, optimizing responsiveness for seamless user experience. Subsequently, the focus extended to laptop and desktop devices, ensuring a consistent and intuitive interface across various screen sizes. This streamlined POS system operates on a simple yet efficient model, with item data sourced from a JSON file. Looking ahead, there are ambitious plans to further enhance StarBuck by integrating a backend system in the near future. This evolution promises to elevate the system's capabilities, enabling it to scale and adapt to the dynamic needs of users. StarBuck stands as a testament to innovation, combining frontend expertise with the vision for future backend integration. \nThe User credentials are; \nEmail:-deelaka@gmail.com, Password:- 123",
+    figma:
+      "https://www.figma.com/file/YWRfRW8hFnzNpTmlCBgjqN/StarBuck-POS-System?type=design&node-id=0%3A1&mode=design&t=JXVDDI030wHHo5bj-1",
+    github: "https://github.com/deelaka99/starbuck",
+    youtube: "https://youtu.be/mbZdM6MVcTk?si=o-XvT3UZLSgJTzKP",
+    activelink: "https://starbuck-pos.netlify.app/",
+    id: 0,
+  },
+  {
+    name: "SharpTester Lab Management System",
+    image: ProjImg8,
+    image1: ProjImg8_1,
+    skills: "ReactJS, Firebase, TailwindCSS, HTML, JS, CSS",
+    specialization: "Web",
+    desc: "This project was a key undertaking within my University Mini Project, where I assumed a leadership role overseeing the development of two integral sites. As part of this collaborative group effort, I took responsibility for the design and implementation of both platforms, and this description pertains to one of those sites. The primary focus of this site is to facilitate the efficient management of users and their associated reports within a specific laboratory in Sri Lanka. Aiming for comprehensive functionality, the platform incorporates essential CRUD operations for both labs and users. Additionally, the system prioritizes security with features such as user authentication and password management, ensuring the confidentiality and integrity of user data. Noteworthy features include a user-friendly dashboard designed to provide analytical insights. This dashboard serves as a centralized hub for users, offering an intuitive interface to explore and understand key analytical details related to the managed data. As the lead contributor to this aspect of the mini project, I navigated the complexities of developing a robust user management system with security features and an analytical dashboard, enhancing the overall functionality and usability of the platform.",
+    figma:
+      "https://www.figma.com/file/japBwlqRJzUXl6XM3lQJap/Laboratory-Dashboard?type=design&node-id=0%3A1&mode=design&t=wkyTHeFT9MO7oV3u-1",
+    github: "https://github.com/deelaka99/lab-admin-panel",
+    youtube: "https://youtu.be/vyH_a4MvQA4",
+    activelink: "https://sharptester-lms.netlify.app/",
+    id: 1,
+  },
+  {
+    name: "DTM Laborties Management System",
+    image: ProjImg5,
+    image1: ProjImg5_1,
+    skills: "ReactJS, Firebase, TailwindCSS, HTML, JS, CSS",
+    specialization: "Web",
+    desc: "This project was a pivotal component of my university's mini-project, where I took on a lead role in managing two integral sites. Focused on enhancing laboratory management in Sri Lanka, this specific site delivers comprehensive functionality, including Create, Read, Update, and Delete (CRUD) operations for both laboratories and users. The primary objective of the platform is to streamline local laboratory management, and it achieves this through robust features. The system enables seamless handling of labs and user data, allowing for efficient CRUD operations. Additionally, the platform prioritizes security with built-in features such as user authentication and password management, providing a secure and user-friendly experience. As part of this initiative, I embraced the responsibility of overseeing and implementing these critical features. Throughout the development process, I navigated the complexities of user authentication, ensuring the system's integrity. Furthermore, the platform incorporates a user-friendly interface for password management, facilitating a convenient and secure means for users to update their passwords. This project not only showcases my technical proficiency in implementing CRUD functionalities but also underscores my commitment to ensuring secure and efficient data management in the context of laboratory administration",
+    figma:
+      "https://www.figma.com/file/ynQFXTGAoRdczP9VYIIkxx/Company-Admin-dashboard?type=design&node-id=0-1&mode=design&t=yzqAJrGmvAF4aCBp-0",
+    github: "https://github.com/deelaka99/company-admin-panel-main",
+    youtube: false,
+    activelink: "https://dtm-admin-panel.netlify.app/",
+    id: 2,
+  },
+  {
+    name: "AT Digital Responsive site",
+    image: ProjImg11,
+    image1: ProjImg11_1,
+    skills: "ReactJS, TailwindCSS, HTML, JS, CSS",
+    specialization: "Web",
+    desc: "This website was crafted as part of a challenging task assigned by AT Digital during the interview selection process for their Frontend Developer Internship Program. Fueled by the determination to showcase my skills, I dedicated 48 hours to create a fully responsive site that not only meets the requirements but also reflects my passion for frontend development. Explore the results of this intensive effort and witness the seamless integration of design and functionality that defines my commitment to excellence in web development.",
+    figma:
+      "https://www.figma.com/file/7jb6yFxNhUwHDgAUQzzIpN/Front-end-Dev-Project?type=design&node-id=2468-430&mode=design&t=Sm39ykfQayZjRiyh-0",
+    github: "https://github.com/deelaka99/AT_Digital_site",
+    youtube: false,
+    activelink: "https://at-digital-site.netlify.app/",
+    id: 3,
+  },
+  {
+    name: "Restaurent Management System",
+    image: ProjImg2,
+    image1: ProjImg2_1,
+    skills:
+      "HTML, CSS, JavaScript, PHP, SQL, Bootstrap, Laravel, MySQL, PhpMyAdmin",
+    specialization: "Web",
+    desc: "This project represents a significant milestone in my academic journey as it was part of a University Mini Project undertaken in the second year. Serving as a collaborative group effort, I assumed the pivotal role of overseeing the development of the entire site.The project is structured into two distinct components: the Admin Dashboard and the User Portal. The Admin Dashboard empowers administrators with comprehensive CRUD (Create, Read, Update, Delete) functionalities. Admins can effortlessly manage the platform by adding and deleting food items, incorporating new chefs, receiving and reviewing orders, and performing various administrative tasks. On the other hand, the User Portal provides a seamless experience for users. They can effortlessly add items to their cart and submit orders, streamlining the process of selecting and purchasing items from the platform. As the primary developer, my responsibilities extended to crafting both the Admin Dashboard and the User Portal, ensuring that each component fulfilled its designated functions effectively. This project not only honed my technical skills but also provided valuable experience in collaborative development within a team setting.",
+    figma: false,
+    github: "https://github.com/deelaka99/Restaurent_Management_System",
+    youtube: false,
+    activelink: false,
+    id: 4,
+  },
+  {
+    name: "To Do Web App",
+    image: ProjImg13,
+    image1: ProjImg13_1,
+    skills: "HTML, CSS, JavaScript, MongoDB, Node.js, React.js, TailwindCSS",
+    specialization: "Web",
+    desc: "This project is my first MERN stack project. Acctually, it was a great pleasure to get an insightful experience when coding both frontend and backend side as a web developer.  Also, i depolyed this using Vercel. From deploying also, i got a good knowledge and experience in how to deploy the frontend part and backend part, and how to connect those two parts . Also, in here i learned about 4 APIs about get, post, put and delete.",
+    figma:
+      "https://www.figma.com/file/INFMAAJFovHI3Fot3il2Vd/MERN_TO_DO_app?type=design&node-id=0%3A1&mode=design&t=LUn9w4YU8xv54Yky-1",
+    github: "https://github.com/deelaka99/ToDoWebApp_using_MERN",
+    youtube: "https://youtu.be/x-cLwqiqQJc",
+    activelink: "https://to-do-web-app-frontend-self.vercel.app",
+    id: 5,
+  },
+  {
+    name: "Portfolio with Dark theme",
+    image: ProjImg7,
+    image1: ProjImg7_1,
+    skills: "ReactJS, HTML, CSS, JavaScript",
+    specialization: "Web",
+    desc: "This project marks a significant milestone as my inaugural venture into ReactJS development. Undertaking this project proved to be an invaluable learning experience, allowing me to gain substantial insights into the intricacies of ReactJS and Tailwind CSS. Despite the realization that the project may not have achieved the level of excellence I initially envisioned, the journey was nonetheless enriching, and I consider the outcome to be a commendable achievement. This project served as a foundational step in my ReactJS journey, laying the groundwork for future endeavors and showcasing my dedication to continuous learning and improvement.",
+    figma: false,
+    github: "https://github.com/deelaka99/dee-wej-kar-portfolio",
+    youtube: false,
+    activelink: "https://dee-wej-kar.netlify.app/",
+    id: 6,
+  },
+  {
+    name: "Glamoure.360 Online Shopping Mall",
+    image: ProjImg6,
+    image1: ProjImg6_1,
+    skills: "HTML, CSS, JavaScript, PHP",
+    specialization: "Web",
+    desc: "This commercial website stands as a testament to my inaugural venture into the world of web development, serving as my first project for a shopping mall. Executed as part of my first-year mini project at OUSL, this website was crafted based on my original design and implementation plan. Taking on the challenge of creating a digital presence for a shopping mall allowed me to apply theoretical knowledge gained in my studies and embark on a hands-on learning experience. This project marks a pivotal moment in my journey, setting the foundation for subsequent web development endeavors and fostering a passion for creating impactful online solutions.",
+    figma: false,
+    github: "https://github.com/deelaka99/Glamoure.360",
+    youtube: false,
+    activelink: "https://deelaka99.github.io/Glamoure.360/",
+    id: 7,
+  },
+  {
+    name: "My Bio Website",
+    image: ProjImg4,
+    image1: ProjImg4_1,
+    skills: "HTML, CSS, JavaScript, PHP, Bootstrap, Adobe Photoshop",
+    specialization: "Web",
+    desc: "This portfolio represents my initial foray into showcasing my skills and creative endeavors. It features a collection of my artworks, photography, and a dedicated site for my mobile photography. While it may be modest in its scope, this portfolio serves as a meaningful introduction to my artistic expressions and passion for photography. As my inaugural portfolio, it reflects the beginning of my journey in presenting my work to a wider audience, setting the stage for future expansions and refinements.",
+    figma: false,
+    github: "https://github.com/deelaka99/myBioWebsite",
+    youtube: false,
+    activelink: "https://deelaka99.github.io/myBioWebsite/",
+    id: 8,
+  },
+  {
+    name: "Weather web app",
+    image: ProjImg15,
+    image1: ProjImg15_1,
+    skills: "ReactJS, CSS, OpenWeather API",
+    specialization: "Web",
+    desc: "A simple weather app which was created using ReactJS. Also, the weather data is fetched from the openWeatherMap API😉👌",
+    figma: false,
+    github: "https://github.com/deelaka99/weatherApp",
+    youtube: false,
+    activelink: "https://deewej-weather-app.netlify.app",
+    id: 9,
+  },
+  {
+    name: "Calculator web app",
+    image: ProjImg14,
+    image1: ProjImg14_1,
+    skills: "HTML, CSS, JavaScript",
+    specialization: "Web",
+    desc: "This is a simple calculator which is created using HTML, CSS and JS.😉👌",
+    figma: false,
+    github: "https://github.com/deelaka99/Calculator-using-HTML-CSS-JS",
+    youtube: false,
+    activelink: "https://simple-calculator-online.netlify.app/",
+    id: 10,
+  },
+  {
+    name: "Fit360 Fitness Tracker App",
+    image: ProjImg1,
+    image1: ProjImg1_1,
+    skills: "Java, XML, Android Studio",
+    specialization: "Mobile",
+    desc: "The Fitness Tracking App is a holistic solution designed for individuals keen on enhancing their fitness and overall health. This application empowers users to seamlessly monitor and elevate their fitness levels through features such as workout tracking, goal setting, progress monitoring, and insightful analytics. Engineered to be user-friendly and intuitive, the app caters to users of all fitness backgrounds, ensuring accessibility and usability. Developed using Android Studio and the Java programming language, this application reflects a commitment to delivering a robust and user-centric fitness experience.",
+    figma: false,
+    appLink:
+      "https://drive.google.com/file/d/1LJ7R3UIVlxWm7oEVRuCpq0RSHXrvjvY_/view?usp=sharing",
+    github: "https://github.com/deelaka99/Fit360",
+    youtube: "https://youtu.be/4oT-3CXrkE4",
+    activelink: false,
+    id: 11,
+  },
+  {
+    name: "To_Do_List app",
+    image: ProjImg3,
+    image1: ProjImg3_1,
+    skills: "Java, XML, Android Studio, SQLite",
+    specialization: "Mobile",
+    desc: "The To-Do App, crafted with Android Studio and powered by SQLite, represents an assignment undertaken during my second year in the OUSL Android subject. This application serves as a practical implementation of fundamental Android development concepts, featuring essential CRUD (Create, Read, Update, Delete) operations for the seamless management of daily tasks. Designed with simplicity in mind, the app provides an intuitive user experience, demonstrating my proficiency in mobile app development and database integration.",
+    figma: false,
+    appLink: false,
+    github: "https://github.com/deelaka99/To_Do_List",
+    youtube: "https://youtu.be/TGr-Z4ANOpE",
+    activelink: false,
+    id: 12,
+  },
+  {
+    name: "Report Analyzing App",
+    image: ProjImg12,
+    image1: ProjImg12_1,
+    skills: "React Native, NativewindCSS, Firebase",
+    specialization: "Mobile",
+    desc: "The SharpTester app is designed to empower end-users with a comprehensive health analysis based on a systematic comparison of their previous health reports. This dynamic application leverages the robust capabilities of Firebase, incorporating real-time database functionality and secure authentication protocols. The app seamlessly interfaces with the Sharptester lab admin panel, establishing a streamlined process for retrieving health reports exclusively from a designated and registered laboratory. Developed using React Native and enhanced with Nativewind for a responsive and intuitive user interface, the SharpTester app ensures a user-friendly experience while prioritizing the privacy and accuracy of health information. Through this innovative mobile solution, individuals can gain valuable insights into their health status, facilitated by the seamless integration of cutting-edge technologies.",
+    figma:
+      "https://www.figma.com/file/6tODPMLgAq0k9D3TQKIvJW/Mobile-app?type=design&node-id=0-1&mode=design&t=7vPc9jSbcTPMYcVQ-0",
+    appLink: false,
+    github: "https://github.com/deelaka99/SharpTester",
+    youtube: false,
+    activelink: false,
+    id: 13,
+  },
+  {
+    name: "Coffee-machine",
+    image: ProjImg9,
+    image1: ProjImg9_1,
+    skills: "Python",
+    specialization: "Code",
+    desc: "This project, undertaken during my first year in the OUSL Python subject, represents a mini project that delves into the realms of object-oriented programming (OOP). Specifically, the project features a code-line-based interface designed for the operation of a coffee machine. Leveraging OOP concepts, I crafted a robust and efficient system that showcases my understanding and application of Python programming principles. Through this endeavor, I not only gained valuable insights into Python development but also honed my skills in designing effective and structured solutions.",
+    figma: false,
+    github: "https://github.com/deelaka99/Coffee-machine",
+    youtube: false,
+    activelink: false,
+    id: 14,
+  },
+  {
+    name: "Library Management System",
+    image: ProjImg16,
+    image1: ProjImg16_1,
+    skills: "C language",
+    specialization: "Code",
+    desc: "This project, undertaken during my first year in the OUSL Structured Programming subject, represents a mini project that delves into the realms of Function-oriented programming. Specifically, the project features a code-line-based interface designed for the operation of a LMS.",
+    figma: false,
+    github: "https://github.com/deelaka99/Library-Mangement-System-using-C",
+    youtube: false,
+    activelink: false,
+    id: 15,
+  },
+];
