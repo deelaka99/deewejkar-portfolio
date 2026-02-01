eefe8cfa5c590081bbbc006fef557105d1422a75
deelaka99 <123wejith@gmail.com>
2026-02-01 16:34:14 +0530

    Created skills  Action and Reducer files

---
+++ src/store/actions/skillsActions.js
@@ -0,0 +1,36 @@
+export const FETCH_SKILLS_BEGIN = "FETCH_SKILLS_BEGIN";
+export const FETCH_SKILLS_SUCCESS = "FETCH_SKILLS_SUCCESS";
+export const FETCH_SKILLS_FAILURE = "FETCH_SKILLS_FAILURE";
+
+export const fetchSkillsBegin = () => ({
+  type: FETCH_SKILLS_BEGIN,
+});
+
+export const fetchSkillsSuccess = (skills) => ({
+  type: FETCH_SKILLS_SUCCESS,
+  payload: skills,
+});
+
+export const fetchSkillsFailure = (error) => ({
+  type: FETCH_SKILLS_FAILURE,
+  payload: { error },
+});
+
+export const fetchSkills = () => {
+  return async (dispatch) => {
+    dispatch(fetchSkillsBegin());
+
+    try {
+      const response = await fetch(
+        "http://localhost:1337/api/skills?filters[type]=technical-skill&populate=*",
+      );
+      const data = await response.json();
+
+      console.log("Data: ", data);
+
+      dispatch(fetchSkillsSuccess(data));
+    } catch (error) {
+      dispatch(fetchSkillsFailure({ error: error.message }));
+    }
+  };
+};
