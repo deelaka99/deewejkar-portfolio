87219d5efb7284257056cf37da697e3c02c32a2c
deelaka99 <123wejith@gmail.com>
2026-02-03 23:07:15 +0530

    Created special moments actions and reducers for fetch  data

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import skillsReducer from "./skillsReducer";
 import experiencesReducer from "./experiencesReducer";
 import projectsReducer from "./projectsReducer";
+import specialMomentsReducer from "./specialMomentsReducer";
 
 const rootReducer = combineReducers({
   skillsReducer,
   experiencesReducer,
   projectsReducer,
+  specialMomentsReducer,
 });
 
 export default rootReducer;
